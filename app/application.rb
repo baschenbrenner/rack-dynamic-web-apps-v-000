df1143ae39f0b0c83edf017de0902c552da3cbf4
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-02 14:11:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,10 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
+    resp.write "First number is: #{num_1}\n"
+    resp.write "Second number is:#{num_2}\n"
+    resp.write "Third number is:#{num_3}\n"
+    
     if num_1==num_2 && num_2==num_3
       puts "You Win"
     else
